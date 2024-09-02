f94738ccc7d4ef39f089cc551c4e062fa09c2c16
Vandewaeter Fabio <vandewaeter.fabio@gmail.com>
2024-09-02 17:58:38 +0200

    fix 1

--- src/main.cpp
+++ src/main.cpp
@@ -4,28 +4,10 @@ Game game;
 Uint64 frequency = SDL_GetPerformanceFrequency();
 
 Uint64 getNormalizedTick(){
-    return SDL_GetPerformanceCounter() - frequency;
+    //std::cout << frequency << " " << SDL_GetPerformanceCounter() / frequency << " " << game.frameDelay << std::endl;
+    return SDL_GetPerformanceCounter() / (frequency/10000000);
 }
 
-// if the loop lasted less than on frame at the speed of game.fixedUPS, wait until the end
-/*void handleTickSpeed(Uint64 &frameStart)
-{
-    Uint64 deltaTime = SDL_GetPerformanceCounter() - frameStart;
-    
-    if (deltaTime < game.frameDelay)
-    {
-        Uint64 remainingTime = game.frameDelay - deltaTime;
-        if (remainingTime >= 20000) // 20000 = 2ms because SDL_Delay(1) wait more than 1 ms ; the last millisecond is handled by the more precise while loop
-        {
-            SDL_Delay((remainingTime / 10000) - 1); // -1 because of the margin of error or SDL_Delay()
-        }
-        deltaTime = SDL_GetPerformanceCounter() - frameStart;
-        while (deltaTime < game.frameDelay) // wait for sub-millisecond duration but is CPU heavy
-        {
-            deltaTime = SDL_GetPerformanceCounter() - frameStart;
-        }
-    }
-}*/
 // if the loop lasted less than on frame at the speed of game.fixedUPS, wait until the end
 void handleTickSpeed(Uint64 &frameStart)
 {
@@ -34,6 +16,7 @@ void handleTickSpeed(Uint64 &frameStart)
     if (deltaTime < game.frameDelay)
     {
         Uint64 remainingTime = game.frameDelay - deltaTime;
+        //std::cout << deltaTime << " " << game.frameDelay << " " << remainingTime << std::endl;
         if (remainingTime >= 20000) // 20000 = 2ms because SDL_Delay(1) wait more than 1 ms ; the last millisecond is handled by the more precise while loop
         {
             SDL_Delay((remainingTime / 10000) - 1); // -1 because of the margin of error or SDL_Delay()
@@ -49,6 +32,12 @@ void handleTickSpeed(Uint64 &frameStart)
 int main(int argc, char *argv[]) {
     game.init("TestEngine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
     game.setUPS(120);
+    /*Uint64 t = SDL_GetPerformanceCounter();
+    SDL_Delay(100);
+    std::cout<<SDL_GetPerformanceCounter()-t<<std::endl;*/
+    Uint64 t = getNormalizedTick();
+    SDL_Delay(100);
+    std::cout<<getNormalizedTick()-t<<std::endl;
 
     while (game.running()){
         Uint64 frameStart = getNormalizedTick();
