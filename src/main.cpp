bc7972c7296735f4600b6730268ae1b80f723108
Vandewaeter Fabio <vandewaeter.fabio@gmail.com>
2024-09-02 19:52:49 +0200

    fix tick speed

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@ Game game;
 Uint64 frequency = SDL_GetPerformanceFrequency();
 
 Uint64 getNormalizedTick(){
-    //std::cout << frequency << " " << SDL_GetPerformanceCounter() / frequency << " " << game.frameDelay << std::endl;
     return SDL_GetPerformanceCounter() / (frequency/10000000);
 }
 
@@ -16,7 +15,6 @@ void handleTickSpeed(Uint64 &frameStart)
     if (deltaTime < game.frameDelay)
     {
         Uint64 remainingTime = game.frameDelay - deltaTime;
-        //std::cout << deltaTime << " " << game.frameDelay << " " << remainingTime << std::endl;
         if (remainingTime >= 20000) // 20000 = 2ms because SDL_Delay(1) wait more than 1 ms ; the last millisecond is handled by the more precise while loop
         {
             SDL_Delay((remainingTime / 10000) - 1); // -1 because of the margin of error or SDL_Delay()
@@ -32,12 +30,6 @@ void handleTickSpeed(Uint64 &frameStart)
 int main(int argc, char *argv[]) {
     game.init("TestEngine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
     game.setUPS(120);
-    /*Uint64 t = SDL_GetPerformanceCounter();
-    SDL_Delay(100);
-    std::cout<<SDL_GetPerformanceCounter()-t<<std::endl;*/
-    Uint64 t = getNormalizedTick();
-    SDL_Delay(100);
-    std::cout<<getNormalizedTick()-t<<std::endl;
 
     while (game.running()){
         Uint64 frameStart = getNormalizedTick();
